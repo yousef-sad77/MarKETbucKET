82e09b3aaf59a572f5babd3967be988070f50130
yousef-saddik77 <yousefsaddik77@gmail.com>
2025-05-05 12:55:33 +0300

    marketbucket project go!

---
+++ web/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      rel="stylesheet"
+      href="node_modules/bootstrap/dist/css/bootstrap.min.css"
+    />
+    <script
+      defer
+      src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+    ></script>
+    <title>marketbucket</title> 
+
+</head>
+<body>
+    
+</body>
+</html>
